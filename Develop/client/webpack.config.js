909d495b907796273af62d5e0b62d992999b957e
ShahidHashmi1 <hashmi.pgh@gmail.com>
2022-11-30 17:56:13 -0500

    final commit before deployment

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -46,7 +46,7 @@ module.exports = () => {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 144, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
-            purpose: 'maskable'
+            purpose: 'any'
           },
         ],
       })
